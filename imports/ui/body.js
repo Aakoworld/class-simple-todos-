3354ce2cbbdb6d0151c28bd19fbacbc574c0bd28
madamkofo <remadeinnigeria@outlook.com>
2018-09-11 16:10:04 +0000

    Add incompleteCount helper to body

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -15,8 +15,16 @@ Template.body.onCreated(function bodyOnCreated() {
 Template.body.helpers({
    
 tasks() {
-    
+	const instance = Template.instance();
+    if (instance.state.get('hideCompleted')) {
+      // If hide completed is checked, filter tasks
+      return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
+    }
+    // Otherwise, return all of the tasks
     return Tasks.find({}, { sort: { createdAt: -1 } });
+  },
+  incompleteCount() {
+    return Tasks.find({ checked: { $ne: true } }).count();
   },
 });
 Template.body.events({
@@ -36,5 +44,8 @@ Template.body.events({
  
     // Clear form
     target.text.value = '';
+  },
+  'change .hide-completed input'(event, instance) {
+    instance.state.set('hideCompleted', event.target.checked);
   },
 });
